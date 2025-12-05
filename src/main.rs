f56708e4a474152e9cefef5f68d83b1d24bb79c5
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2025-12-05 18:31:12 -0300

    Initial commit - naive implementation
    ________________________________________________________
    Executed in   83.24 secs    fish           external
       usr time   79.89 secs    0.19 millis   79.89 secs
       sys time    2.52 secs    1.36 millis    2.52 secs

---
+++ src/main.rs
@@ -0,0 +1,142 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct StationStats {
+    min: f64,
+    max: f64,
+    total: f64,
+    count: usize
+}
+
+fn aggregate(filename: &str) -> String {
+    let measurements = BufReader::new(File::open(filename).expect("Need 'measurements.txt' in the current directory."));
+
+    let mut stats = HashMap::<String, StationStats>::new();
+    for line in measurements.lines() {
+        let line = line.expect("Failed to read next line...");
+
+        // Parse line
+        let (station, reading) = line.split_once(';').expect("Bad line structure");
+
+        // Parse reading
+        let reading = reading.parse::<f64>().expect("Not a floating point number");
+
+        // Update tracking
+        stats.entry(station.to_owned()).and_modify(|entry| {
+            if reading < entry.min {
+                entry.min = reading;
+            } else if reading > entry.max {
+                entry.max = reading;
+            }
+
+            entry.total += reading;
+            entry.count += 1;
+        }).or_insert(StationStats { min: reading, max: reading, total: reading, count: 1 });
+    }
+
+    let mut names: Vec<&String> = stats.keys().collect();
+    names.sort();
+
+    let mut output = String::from("{");
+    for (count, station) in names.into_iter().enumerate() {
+        if count != 0 {
+            output.push_str(", ");
+        }
+        let station_stats = stats.get(station).expect("Station should exist in the hashmap");
+        let mean = station_stats.total / station_stats.count as f64;
+
+        // Apply IEEE 754 roundTowardPositive (ceiling) to 1 decimal place
+        let min = (station_stats.min * 10.0).ceil() / 10.0;
+        let max = (station_stats.max * 10.0).ceil() / 10.0;
+        let mean = (mean * 10.0).ceil() / 10.0;
+
+        output.push_str(&format!("{station}={:.1}/{:.1}/{:.1}", min, mean, max));
+    }
+    output.push_str("}\n");
+
+    output
+}
+
+fn main() {
+    let filename = std::env::args().nth(1).unwrap_or_else(|| "measurements.txt".to_string());
+    let output = aggregate(&filename);
+    println!("{}", output);
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::aggregate;
+
+    #[inline(always)]
+    fn check_measurements(basename: &str) {
+        let reference_name = format!("samples/{}.out", basename);
+        let actual_name = format!("samples/{}.txt", basename);
+
+        let reference = String::from_utf8(std::fs::read(reference_name).unwrap()).unwrap();
+        let actual = aggregate(&actual_name);
+
+        assert_eq!(reference, actual);
+    }
+
+    #[test]
+    fn measurements_1() {
+        check_measurements("measurements-1");
+    }
+
+    #[test]
+    fn measurements_10() {
+        check_measurements("measurements-10");
+    }
+
+    #[test]
+    fn measurements_10000_unique_keys() {
+        check_measurements("measurements-10000-unique-keys");
+    }
+
+    #[test]
+    fn measurements_2() {
+        check_measurements("measurements-2");
+    }
+
+    #[test]
+    fn measurements_20() {
+        check_measurements("measurements-20");
+    }
+
+    #[test]
+    fn measurements_3() {
+        check_measurements("measurements-3");
+    }
+
+    #[test]
+    fn measurements_boundaries() {
+        check_measurements("measurements-boundaries");
+    }
+
+    #[test]
+    fn measurements_complex_utf8() {
+        check_measurements("measurements-complex-utf8");
+    }
+
+    #[test]
+    fn measurements_dot() {
+        check_measurements("measurements-dot");
+    }
+
+    #[test]
+    fn measurements_rounding() {
+        check_measurements("measurements-rounding");
+    }
+
+    #[test]
+    fn measurements_short() {
+        check_measurements("measurements-short");
+    }
+
+    #[test]
+    fn measurements_shortest() {
+        check_measurements("measurements-shortest");
+    }
+}
